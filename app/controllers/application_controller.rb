0ea50f8f31d84bf47e0c9fafe5e9cec1564d67cc
Ivan Osadchii <ivan.osadchii@gmail.com>
2011-05-12 23:16:02 +0300

    Don't recall user if he's already logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include InheritedResources::DSL
 
   protect_from_forgery
-  before_filter :try_to_recall_user
+  before_filter :try_to_recall_user, :unless => :user_signed_in?
 
   def try_to_recall_user
     if c = cookies.signed['remember_user_token'] and u = User.serialize_from_cookie(c[0], c[1])
