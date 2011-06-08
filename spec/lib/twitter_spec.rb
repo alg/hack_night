f7f7fa0ab7d3145b658dc4347ddb0fcd3c9feeb1
Ivan Osadchii <ivan.osadchii@gmail.com>
2011-06-08 12:45:50 +0300

    typo

--- spec/lib/twitter_spec.rb
+++ spec/lib/twitter_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'twitterie'
 
-def tw_reponse(name)
+def tw_response(name)
   File.open("spec/fixtures/twitter-responses/#{name}.response")
 end
 
@@ -15,34 +15,34 @@ describe Twitterie do
 
 
   describe 'timeline' do
-    before { tw_stub_request(:get, "statuses/home_timeline").to_return(:body => tw_reponse("statuses-home_timeline.success")) }
+    before { tw_stub_request(:get, "statuses/home_timeline").to_return(:body => tw_response("statuses-home_timeline.success")) }
     specify { Twitterie.timeline.should be_a Array }
   end
 
 
   describe 'send a message' do
     context 'recipient is valid' do
-      before { tw_stub_request(:post, "direct_messages/new").to_return(:body => tw_reponse("direct_messages-new.success")) }
+      before { tw_stub_request(:post, "direct_messages/new").to_return(:body => tw_response("direct_messages-new.success")) }
       specify { lambda { Twitterie.post_direct_message("micky_mouse", "hi there") }.should_not raise_error }
     end
 
     context 'recipient is invalid' do
-      before { tw_stub_request(:post, "direct_messages/new").to_return(:status => 403, :body => tw_reponse("direct_messages-new.forbidden")) }
+      before { tw_stub_request(:post, "direct_messages/new").to_return(:status => 403, :body => tw_response("direct_messages-new.forbidden")) }
       specify { lambda { Twitterie.post_direct_message("tom_the_cat", "hi there") }.should raise_error(Twitterie::ApiError) }
     end
   end
 
 
   describe "send a tweet" do
-    before { tw_stub_request(:post, "statuses/update").to_return(:body => tw_reponse("statuses-update.success")) }
+    before { tw_stub_request(:post, "statuses/update").to_return(:body => tw_response("statuses-update.success")) }
     specify { lambda { Twitterie.update_status("playing nice with the kitten #{Time.now}") }.should_not raise_error }
   end
 
 
   describe "remove all the tweets" do
     specify "should remove every tweet from the timeline" do
-      tw_stub_request(:get, "statuses/home_timeline").to_return(:body => tw_reponse("statuses-home_timeline.success"))
-      stub_request(:post, /http:\/\/api.twitter.com\/1\/statuses\/destroy\/.*/).to_return(:body => tw_reponse("statuses-destroy.success"))
+      tw_stub_request(:get, "statuses/home_timeline").to_return(:body => tw_response("statuses-home_timeline.success"))
+      stub_request(:post, /http:\/\/api.twitter.com\/1\/statuses\/destroy\/.*/).to_return(:body => tw_response("statuses-destroy.success"))
       @tweets = Twitterie.timeline
 
       @tweets.each do |tweet|
